c4fb180db2373f16b2f601a2e48d391cdc222616
linearcombination <4829djaskdfj@gmail.com>
2020-09-07 21:17:26 -0700

    Change from cli interaction to web service
    * Design is moving to having a web front end that handles incoming
    resource requests by launching a Flask web app front end
    in container

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ export IRG_WORKING_DIR="/working/tn-temp"
 export IRG_OUTPUT_DIR="$(pwd)/output"
 export IRG_CS_USER_URL="https://content.bibletranslationtools.org/WycliffeAssociates"
 export IRG_CS_USER_NONENG_URL="https://content.bibletranslationtools.org/WA-Catalog"
-export TN_ADDITIONAL_PARMS="--lang ml --book gen"
+export TN_ADDITIONAL_PARMS="--lang ml --book gen exo"
 docker-compose build --no-cache
 # docker-compose build
 docker-compose up
