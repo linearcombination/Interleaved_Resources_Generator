6f5d81529d92668c306a777077926b6d9ef708b5
linearcombination <4829djaskdfj@gmail.com>
2020-09-12 16:42:38 -0700

    Set IN_CONTAINER env var in build script and docker compose up
    * System relies on value of this env var to determine certain other
    values based on runtime environment

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ export IRG_OUTPUT_DIR="$(pwd)/output"
 export IRG_CS_USER_URL="https://content.bibletranslationtools.org/WycliffeAssociates"
 export IRG_CS_USER_NONENG_URL="https://content.bibletranslationtools.org/WA-Catalog"
 export TN_ADDITIONAL_PARMS="--lang ml --book gen exo"
+export IN_CONTAINER=1 # The this is used, it can be any value other than nothing to indicate container environment
 docker-compose build --no-cache
 # docker-compose build
 docker-compose up
