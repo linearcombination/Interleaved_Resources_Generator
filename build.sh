efd7b1a03d77f86dd8b0005e2c9b1207be5f0a46
linearcombination <4829djaskdfj@gmail.com>
2020-10-27 14:22:24 -0700

    Build dir is /working/temp not /working/tn-temp

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export IRG_WORKING_DIR="/working/tn-temp"
+export IRG_WORKING_DIR="/working/temp"
 export IRG_OUTPUT_DIR="$(pwd)/output"
 export IRG_CS_USER_URL="https://content.bibletranslationtools.org/WycliffeAssociates"
 export IRG_CS_USER_NONENG_URL="https://content.bibletranslationtools.org/WA-Catalog"
